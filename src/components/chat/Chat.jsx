56573b97602770041479df82441e6dbff8de60a1
sandeep0302 <159161078+sandeep0302@users.noreply.github.com>
2024-07-09 15:22:02 +0530

    Details Component

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,9 +1,19 @@
+import { useState } from 'react';
 import './chat.css';
 import EmojiPicker from 'emoji-picker-react';
 
 
 
 const Chat = () => {
+  const[open ,setOpen]=useState(false);
+  const[text,setText]=useState("")
+
+  const handleEmoji = e => {
+    setText((prev)=> prev + e.emoji);
+    setOpen(false)
+  }
+  
+
   return (
     <div className='chat'>
       <div className="top">
@@ -21,17 +31,72 @@ const Chat = () => {
           <img src="./info.png" alt="" />
         </div>
       </div>
-      <div className="center"></div>
+      <div className="center">
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Hello,How are you doing ?
+              what are your plans for today ?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <img src="https://via.placeholder.com/150" alt="" />
+            <p>Hello,How are you doing ?
+              what are your plans for today ?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Hello,How are you doing ?
+              what are your plans for today ?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>Hello,How are you doing ?
+              what are your plans for today ?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>Hello,How are you doing ?
+              what are your plans for today ?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>Hello,How are you doing ?
+              what are your plans for today ?
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+      </div>
       <div className="bottom">
         <div className="icons">
           <img src="./img.png" alt="" />
           <img src="./camera.png" alt="" />
           <img src="./mic.png" alt="" />
         </div>
-        <input type="text" placeholder='type a message...' />
+        <input type="text"  value={text} placeholder='type a message...' onChange={(e)=>setText(e.target.value)}/>
         <div className="emoji">
-          <img src="./emoji.png" alt="" />
-          <EmojiPicker />
+          <img src="./emoji.png" alt=""  onClick={()  => setOpen((prev)=>!prev)}/>
+          <div className="picker">
+          <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
+        </div>
         </div>
         <button className='sendButton'>Send</button>
        
--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -2,7 +2,47 @@ import './detail.css'
 
 const Detail = () => {
   return (
-    <div className='detail'>Detail</div>
+    <div className='detail'>
+      <div className="user">
+        <img src="./avatar.png"  alt="" />
+        <h2>Sampath</h2>
+        <p>Hello,how are you doing these days?</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & Help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+           <div className="photoItem">
+            <img src="https://via.placeholder.com/150" alt="" />
+            <span>photo_2024_2.png</span>
+           </div>
+           <img src="./download.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+      </div>
+    </div>
   )
 }
 
